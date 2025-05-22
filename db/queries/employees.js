594367767b1abc0e716eb843b119866426cf8a1e
kourtney_miranda <mirandakourt@gmail.com>
2025-05-22 15:48:18 -0700

    Complete Workshop 34: Employees schema and seed

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,35 +1,11 @@
-/** @returns the employee created according to the provided details */
-export async function createEmployee({ name, birthday, salary }) {
-  // TODO
-}
+import client from "../client.js";
 
-// === Part 2 ===
-
-/** @returns all employees */
-export async function getEmployees() {
-  // TODO
-}
-
-/**
- * @returns the employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function getEmployee(id) {
-  // TODO
-}
-
-/**
- * @returns the updated employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
-}
-
-/**
- * @returns the deleted employee with the given id
- * @returns undefined if employee with the given id does not exist
- */
-export async function deleteEmployee(id) {
-  // TODO
+export async function createEmployee(name, birthday, salary) {
+  const result = await client.query(
+    `INSERT INTO employees (name, birthday, salary)
+     VALUES ($1, $2, $3)
+     RETURNING *;`,
+    [name, birthday, salary]
+  );
+  return result.rows[0];
 }
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,15 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  await createEmployee("Tony Stark", "1970-05-29", 1000000);
+  await createEmployee("Bruce Banner", "1969-12-18", 900000);
+  await createEmployee("Natasha Romanoff", "1984-11-22", 800000);
+  await createEmployee("Steve Rogers", "1918-07-04", 750000);
+  await createEmployee("Clint Barton", "1971-06-18", 650000);
+  await createEmployee("Peter Parker", "2001-08-10", 500000);
+  await createEmployee("Wanda Maximoff", "1989-02-10", 700000);
+  await createEmployee("Sam Wilson", "1985-03-15", 600000);
+  await createEmployee("Bucky Barnes", "1917-03-10", 720000);
+  await createEmployee("Stephen Strange", "1975-11-11", 950000);
 }
+
